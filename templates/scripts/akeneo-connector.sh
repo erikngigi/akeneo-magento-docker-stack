bd4eb71c381697921b3b662962dc02cfd08faea4
erikngigi <ericmosesngigi@gmail.com>
2025-11-07 10:49:17 +0300

    feat: add akeneo connector install script

--- templates/scripts/akeneo-connector.sh
+++ templates/scripts/akeneo-connector.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Resolve the real path of this script (handles symlinks)
-AKENEO_SCRIPT_DIR="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
+MAGENTO_SCRIPT_DIR="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
 
 # source the file with the magento details
 source "$MAGENTO_SCRIPT_DIR/magento.env"
 
 # Helper shell script for printing
-source "$AKENEO_SCRIPT_DIR/colors.sh"
+source "$MAGENTO_SCRIPT_DIR/colors.sh"
 
 # Define a reusable function for the Docker container magento
 magento_exec() {
