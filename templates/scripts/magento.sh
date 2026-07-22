36b22af7e4613666db48a5f873e037562298652d
erikngigi <ericmosesngigi@gmail.com>
2026-07-22 22:01:27 +0300

    fix: change magento script installation values

--- templates/scripts/magento.sh
+++ templates/scripts/magento.sh
@@ -55,16 +55,16 @@ bin/magento setup:install \
 --use-rewrites=1 \
 --elasticsearch-host=$ELASTICSEARCH_HOST \
 --elasticsearch-port=$ELASTICSEARCH_PORT \
---session-save=$REDIS_HOST \
---session-save-redis-host=$REDIS_HOST \
+--session-save=redis \
+--session-save-redis-host=redis \
 --session-save-redis-db=0 \
 --session-save-redis-password=${REDIS_PASSWORD} \
---cache-backend=$REDIS_HOST \
---cache-backend-redis-server=$REDIS_HOST \
+--cache-backend=redis \
+--cache-backend-redis-server=redis \
 --cache-backend-redis-db=2 \
 --cache-backend-redis-password=${REDIS_PASSWORD} \
 --page-cache=redis \
---page-cache-redis-server=$REDIS_HOST \
+--page-cache-redis-server=redis \
 --page-cache-redis-db=4 \
 --page-cache-redis-password=${REDIS_PASSWORD}
 "
