92497c61b7de49af365b112fb629fb83de25c462
erikngigi <ericmosesngigi@gmail.com>
2026-07-22 13:59:26 +0300

    test: uncomment akeneo installation script process

--- templates/scripts/akeneo.sh
+++ templates/scripts/akeneo.sh
@@ -22,30 +22,30 @@ print_green "Extract Akeneo PIM archive"
 akeneo_exec bash -c "tar -xzf /tmp/akeneo.tar.gz -C $AKENEO_INST_DIR --strip-components=1 && rm -rf /tmp/akeneo.tar.gz"
 
 # copy .env to .env.local
-# print_green "Creating Akeneo .env.local file..."
-# akeneo_exec bash -c "cd $AKENEO_INST_DIR && cp .env .env.local"
+print_green "Creating Akeneo .env.local file..."
+akeneo_exec bash -c "cd $AKENEO_INST_DIR && cp .env .env.local"
 
 # create an executable akeneo console application
-# akeneo_exec bash -c "cd $AKENEO_INST_DIR && chmod u+x bin/console"
+akeneo_exec bash -c "cd $AKENEO_INST_DIR && chmod u+x bin/console"
 
 # append details to .env.local file
-# print_green "Appending details to the Akeneo .env.local file"
-# akeneo_exec bash -c "
-#     cd $AKENEO_INST_DIR &&
-#     sed -i 's/^APP_DATABASE_HOST=.*/APP_DATABASE_HOST=$MYSQL_AKENEO_DATABASE_HOST/' .env.local;
-#     sed -i 's/^APP_DATABASE_NAME=.*/APP_DATABASE_NAME=$MYSQL_AKENEO_DATABASE/' .env.local;
-#     sed -i 's/^APP_DATABASE_USER=.*/APP_DATABASE_USER=$MYSQL_AKENEO_USER/' .env.local;
-#     sed -i 's/^APP_DATABASE_PASSWORD=.*/APP_DATABASE_PASSWORD=$MYSQL_AKENEO_PASSWORD/' .env.local;
-#     sed -i 's/^APP_INDEX_HOSTS=.*/APP_INDEX_HOSTS=$APP_INDEX_HOSTS/' .env.local;
-# "
+print_green "Appending details to the Akeneo .env.local file"
+akeneo_exec bash -c "
+    cd $AKENEO_INST_DIR &&
+    sed -i 's/^APP_DATABASE_HOST=.*/APP_DATABASE_HOST=$MYSQL_AKENEO_DATABASE_HOST/' .env.local;
+    sed -i 's/^APP_DATABASE_NAME=.*/APP_DATABASE_NAME=$MYSQL_AKENEO_DATABASE/' .env.local;
+    sed -i 's/^APP_DATABASE_USER=.*/APP_DATABASE_USER=$MYSQL_AKENEO_USER/' .env.local;
+    sed -i 's/^APP_DATABASE_PASSWORD=.*/APP_DATABASE_PASSWORD=$MYSQL_AKENEO_PASSWORD/' .env.local;
+    sed -i 's/^APP_INDEX_HOSTS=.*/APP_INDEX_HOSTS=$APP_INDEX_HOSTS/' .env.local;
+"
 
 # Update browserlist
-# print_green "Updating Browserlist for Akeneo"
-# akeneo_exec bash -c "cd $AKENEO_INST_DIR && yes | npx update-browserslist-db@latest"
+print_green "Updating Browserlist for Akeneo"
+akeneo_exec bash -c "cd $AKENEO_INST_DIR && yes | npx update-browserslist-db@latest"
 
 # Install akeneo
-# print_blue "Installing Akeneo"
-# akeneo_exec bash -c "cd $AKENEO_INST_DIR && NO_DOCKER=true make dev"
+print_blue "Installing Akeneo"
+akeneo_exec bash -c "cd $AKENEO_INST_DIR && NO_DOCKER=true make dev"
 
-# print_green "Creating User $AKENEO_USERNAME"
-# akeneo_exec bash -c "cd $AKENEO_INST_DIR && bin/console pim:user:create $AKENEO_USERNAME $AKENEO_PASSWORD $AKENEO_USER_EMAIL $AKENEO_FIRST_NAME $AKENEO_LAST_NAME $AKENEO_LOCALE --admin -n --env=dev"
+print_green "Creating User $AKENEO_USERNAME"
+akeneo_exec bash -c "cd $AKENEO_INST_DIR && bin/console pim:user:create $AKENEO_USERNAME $AKENEO_PASSWORD $AKENEO_USER_EMAIL $AKENEO_FIRST_NAME $AKENEO_LAST_NAME $AKENEO_LOCALE --admin -n --env=dev"
